d541c5d61fa01e3da2b3af1e31557a5d01b6db30
moodkip <moodkip.shazaam@gmail.com>
2016-04-13 14:30:56 -0600

    Josh's Contributions - Level

---
+++ Assets/demo/Trigger_Script.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+
+public class Trigger_Script : MonoBehaviour {
+
+    public Transform Obj2_Activate;
+    public CharacterController2D controller;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Awake()
+    {
+        controller.onTriggerEnterEvent += ActivateObject;
+    }
+    
+    void ActivateObject(Collider2D col)
+    {
+        if (Obj2_Activate.gameObject.activeSelf == false)
+        {
+            Obj2_Activate.gameObject.SetActive(true);
+        }
+    }
+}
