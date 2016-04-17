b7f572788c0f9424d6fabc4357a94daa60f32b3d
Adam Kay <kayad01@gettysburg.edu>
2016-04-16 19:11:47 -0600

    Added Enemy prefab

---
+++ Assets/PlayerFinder.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
+
+public class PlayerFinder : MonoBehaviour {
+
+    private CharacterController2D _controller;
+
+    void Awake()
+    {
+        _controller = this.gameObject.GetComponentInParent<CharacterController2D>();
+        _controller.onTriggerEnterEvent += SearchPlayer;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SearchPlayer(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SendMessageUpwards("FoundPlayer");
+        }
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,19 +6,23 @@ using Prime31;
 public class Enemy : MonoBehaviour, IHealth, IKillable {
     public int health;
     public int maxHealth;
+    public bool foundPlayer = false;
+    public float walkSpeed;
 
     private Vector3 _velocity;
 
-    public float deathGravity = -10;
+    public float gravity;
     public float deathJumpHeight = 5;
 
     public bool isDead = false;
 
     private CharacterController2D _controller;
+    public SpriteAnimator _animator;
 
     void Awake()
     {
         _controller = GetComponent<CharacterController2D>();
+        
         _velocity = _controller.velocity;
     }
 
@@ -26,6 +30,12 @@ public class Enemy : MonoBehaviour, IHealth, IKillable {
     {
         health -= amount;
         if (health <= 0) Kill();
+    }
+
+    public void FoundPlayer()
+    {
+        foundPlayer = true;
+        _animator.Play("EnemyWalk", true);
     }
 
     public void Heal(int amount)
@@ -36,48 +46,57 @@ public class Enemy : MonoBehaviour, IHealth, IKillable {
 
     public void Kill()
     {
-        _controller.platformMask = LayerMask.GetMask("CameraPlatform");
-        Transform cameraPlatform = GameObject.FindGameObjectWithTag("CameraPlatform").transform;
-        transform.SetParent(cameraPlatform, true);
-        _velocity.y = Mathf.Sqrt(2f * deathJumpHeight * -deathGravity);
-        gameObject.layer = 12;
-        
+        _animator.Play("EnemyDeath");
         isDead = true;
         
     }
 
+    public void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+
     // Use this for initialization
     void Start () {
-	
+        // _animator.Play("EnemyIdle");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
-
 
         if (isDead)
         {
-            if (!_controller.isGrounded)
+            _velocity.x = 0;
+            _velocity.y = 0;
+        }
+
+
+        if (foundPlayer && !isDead) {
+            if (transform.localScale.x < 0)
             {
-                _velocity.y += deathGravity * Time.deltaTime;
+                _velocity.x = walkSpeed;
             }
             else
             {
- //               _velocity.y = 0;
+                _velocity.x = walkSpeed * -1;
             }
         }
         else
         {
-            if (!_controller.isGrounded)
-            {
-                _velocity.y += deathGravity * Time.deltaTime;
-            }
-            else
-            {
-                // _velocity.y = 0;
-            }
+            
+            
         }
+
+        if (!_controller.isGrounded && !isDead)
+        {
+            _velocity.y += gravity * Time.deltaTime;
+        }
+        else
+        {
+            _velocity.y = 0;
+        }
+
+
 
         _velocity *= Time.deltaTime;
         _controller.move(_velocity);
