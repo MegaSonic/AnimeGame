eb56b778d9355d49e1abbeda53e4927b4f2eb6cc
Adam Kay <kayad01@gettysburg.edu>
2016-04-16 22:16:53 -0600

    Working on title screen animations

---
+++ Assets/MusicOuttro.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MusicOuttro : MonoBehaviour {
+
+    public AudioSource platinum;
+    public AudioSource platinumStinger;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void StartStinger()
+    {
+        platinum.Stop();
+        platinumStinger.Play();
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene("Episode");
+    }
+}
---
+++ Assets/NewGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGame : MonoBehaviour {
+
+    public Animation _animation;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartNewGame()
+    {
+        _animation.Play();
+    }
+}
