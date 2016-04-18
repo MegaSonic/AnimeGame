ade2cc199d8a2e5465af5e09837c75c9775cbc43
Adam Kay <kayad01@gettysburg.edu>
2016-04-17 19:32:00 -0600

    Sync

---
+++ Assets/LevelMusic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelMusic : MonoBehaviour {
+
+    public AudioSource music1;
+    public AudioSource music2;
+
+    bool musicSwitch = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (music1.isPlaying == false && musicSwitch == false)
+        {
+            music2.Play();
+            musicSwitch = true;
+        }
+        else if (music2.isPlaying == false && musicSwitch == true)
+        {
+            music1.Play();
+            musicSwitch = false;
+        }
+	}
+}
